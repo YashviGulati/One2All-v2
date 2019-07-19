9c5df0eb1c7e160d2105183320f09341fccc7ca6
Yashvi Gulati <32419771+YashviGulati@users.noreply.github.com>
2019-07-19 11:13:55 +0530

    added comments

--- main.go
+++ main.go
@@ -21,6 +21,39 @@ func main(){
 	y.Handle("/topics/{name}", basicauthmiddleware.BasicAuthMiddleware(http.HandlerFunc(handlecontrol.DeleteTopicByName))).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":1010", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(y)))
+	}
 
+/*
+Documentation:
 
-}
+func NewRouter():
+returns a new router instance.
+
+func HandleFunc():
+registers a new route with a matcher for the URL path
+HandleFunc expects a function
+
+func Handle():
+Handle expects a Handler.
+
+func Fatal():
+similar to printf()
+
+func ListenAndServe():
+ListenAndServe listens on the TCP network address and
+then calls Serve with handler to handle requests on incoming connections.
+
+func CORS():
+CORS provides Cross-Origin Resource Sharing middleware.
+
+func AllowedHeaders():
+adds the provided headers to the list of allowed headers in a CORS request
+
+func AllowedMethods():
+AllowedMethods can be used to explicitly allow methods in the Access-Control-Allow-Methods header.
+
+func AllowedOrigins():
+AllowedOrigins sets the allowed origins for CORS requests, as used in the 'Allow-Access-Control-Origin'
+Passing in a []string{"*"} will allow any domain.
+
+*/
